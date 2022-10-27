6fc85c8df78eb8230283f1e5e2dc390955658a5b
Colin Stephenson <colinjeremiahbernard@gmail.com>
2022-10-27 10:54:52 -0300

    progress at last

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import NotFound from './pages/NotFound'
+
+export default() => {
+    return(
+        <Routes>
+            <Route exact path="/" element = {<Home/>}/>
+            <Route exact path="/about" element = {<About/>}/>
+            <Route exact path="/signin" element = {<SignIn/>}/>
+            <Route exact path="/signup" element = {<SignUp/>}/>
+            <Route path="*" element= {<NotFound/>}/>
+        </Routes>
+    )
+}
---
+++ src/helpers/shopAPI.js
@@ -0,0 +1,10 @@
+import Cookies from 'js-cookie'
+import qs from 'qs'
+
+const BASEAPI = '';
+const shopAPI = {
+    login: async(email, password) => {
+        return{ };
+    }
+}
+export default () => shopAPI;
